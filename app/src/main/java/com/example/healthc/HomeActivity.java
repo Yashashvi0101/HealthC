40bd5e0dda72cab8d1c118c69ff603aa01dc4ade
Yashashvi0101 <yashashvi0101@gmail.com>
2024-09-29 17:33:44 +0530

    Final commit

--- app/src/main/java/com/example/healthc/HomeActivity.java
+++ app/src/main/java/com/example/healthc/HomeActivity.java
@@ -10,9 +10,9 @@ import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+//import androidx.core.graphics.Insets;
+//import androidx.core.view.ViewCompat;
+//import androidx.core.view.WindowInsetsCompat;
 
 public class HomeActivity extends AppCompatActivity {
 
